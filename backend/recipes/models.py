e463ba01d009c7d0fee27a6e2c9fd0eaa06e7000
Shamil Zaylyalov <Zaylyalow@gmail.com>
2023-10-07 06:43:19 +0500

    Fix recipes.models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.core.validators import (RegexValidator, MinValueValidator,
                                     MaxValueValidator)
 from colorfield.fields import ColorField
